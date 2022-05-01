9e3087d0b4985b445e743842b5bcdefa254df62f
student63279797 <63279797@mylife.unisa.ac.za>
2022-05-01 17:37:22 +0200

    initial

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // float var1,var2;
+    // string operation;
+    // cout << "Please enter the first float value:";
+    // cin>>var1;
+    // cout << "Please enter the second float value:";
+    // cin>>var2;
+    // cout << "Please enter the operation required :";
+    // cin>>operation;
+
+    // if (operation == "+")
+    // {
+    //     float sum = var1+var2;
+    //     cout << "The sum of " << var1 <<" and " << var2 <<" is " << sum;
+    // }
+    // if (operation == "-")
+    // {
+    //     float difference = var1-var2;
+    //     cout << "The difference of " << var1 <<" and " << var2 <<" is " << difference;
+    // }
+    // if (operation == "*")
+    // {
+    //     float product = var1*var2;
+    //     cout << "The product of " << var1 <<" and " << var2 <<" is " << product;
+    // }
+    // if (operation == "/")
+    // {
+    //     float division = var1/var2;
+    //     cout << "The division of " << var1 <<" and " << var2 <<" is " << division;
+    // }
+    // if (operation == "%")
+    // {
+    //     int modulo = (int)var1 % (int)var2;
+    //     cout << "The modulos of " << var1 <<" and " << var2 <<" is " << modulo;
+    // }
+    
+    return 0;
+}
