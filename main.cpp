4e9f8064eec3f2cc3bf509a3f39deb0cd67e0ab3
student63279797 <63279797@mylife.unisa.ac.za>
2022-05-01 17:53:41 +0200

    solution

--- main.cpp
+++ main.cpp
@@ -5,40 +5,15 @@ using namespace std;
 
 int main()
 {
-    // float var1,var2;
-    // string operation;
-    // cout << "Please enter the first float value:";
-    // cin>>var1;
-    // cout << "Please enter the second float value:";
-    // cin>>var2;
-    // cout << "Please enter the operation required :";
-    // cin>>operation;
-
-    // if (operation == "+")
-    // {
-    //     float sum = var1+var2;
-    //     cout << "The sum of " << var1 <<" and " << var2 <<" is " << sum;
-    // }
-    // if (operation == "-")
-    // {
-    //     float difference = var1-var2;
-    //     cout << "The difference of " << var1 <<" and " << var2 <<" is " << difference;
-    // }
-    // if (operation == "*")
-    // {
-    //     float product = var1*var2;
-    //     cout << "The product of " << var1 <<" and " << var2 <<" is " << product;
-    // }
-    // if (operation == "/")
-    // {
-    //     float division = var1/var2;
-    //     cout << "The division of " << var1 <<" and " << var2 <<" is " << division;
-    // }
-    // if (operation == "%")
-    // {
-    //     int modulo = (int)var1 % (int)var2;
-    //     cout << "The modulos of " << var1 <<" and " << var2 <<" is " << modulo;
-    // }
-    
+    int programsDone = 0,result=0;
+    while (programsDone < 5 && result < 50)
+    {
+        cout << "Please enter amount of completed programs.";
+        cin>>programsDone;
+        cout << "Please enter mark obtained.";
+        cin>>result;
+    }
+    cout << "Good! You can now proceed to the next exercise.";
+        
     return 0;
 }
